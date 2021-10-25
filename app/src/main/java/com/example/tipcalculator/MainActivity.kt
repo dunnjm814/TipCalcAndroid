78e35dd84b5478757800a043c637777ab4ecd86e
Jason Dunn <dunnjm814@gmail.com>
2021-10-24 17:06:53 -0700

    enabled binding in build scripts, and initialized binding object in main

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    // lateinit keyword promises your code will initialize the variable before using it
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // initializes binding object which we will access Views from activity_main.xml
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // Set content view of activity. Instead of passing ID resource of layout, R.layout.activity_main
+        // specify root of app view heirarchy to binding root
+        setContentView(binding.root)
     }
 }
