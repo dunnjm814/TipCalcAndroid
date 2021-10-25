0492ed36361256bb9fd2a5d6129e7131124f8a07
Jason Dunn <dunnjm814@gmail.com>
2021-10-24 17:30:18 -0700

    main app logic in place, app shuts down if null input

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tipcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +14,30 @@ class MainActivity : AppCompatActivity() {
         // initializes binding object which we will access Views from activity_main.xml
         binding = ActivityMainBinding.inflate(layoutInflater)
         // Set content view of activity. Instead of passing ID resource of layout, R.layout.activity_main
-        // specify root of app view heirarchy to binding root
+        // specify root of app view hierarchy to binding root
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    fun calculateTip() {
+        // grab text from EditText view, since this is an Editable, we must cast it into a string()
+        val stringInTextField = binding.costOfService.text.toString()
+        // convert string into a decimal number.
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        // use of var here instead of val because we may need to round up if switch is checked true
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        // formatting the tip double into a currency
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
